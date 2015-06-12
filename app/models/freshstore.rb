484231e806a44c9c6dd1e8035f5bf5bbfbb2fc37
Hilary Sinkoff <hsinkoff@hotmail.com>
2015-06-12 00:55:38 +0000

    remove unused database tables

--- app/models/freshstore.rb
+++
@@ -1,2 +0,0 @@
-class Freshstore < ActiveRecord::Base
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,28 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150607044624) do
+ActiveRecord::Schema.define(version: 20150612005355) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "foods", force: true do |t|
-    t.string   "place_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.text     "fruits",     default: [], array: true
-    t.text     "veggies",    default: [], array: true
-  end
-
-  create_table "freshstores", force: true do |t|
-    t.string   "name"
-    t.string   "address"
-    t.string   "zipcode"
-    t.float    "latitude"
-    t.float    "longitude"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
   create_table "stores", force: true do |t|
     t.string   "name"
